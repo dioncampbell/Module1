9f1b45e4a7d32c0cf7e4c108eaa88f4eb067e7fb
dioncampbell <dion.campbell@csuglobal.edu>
2024-09-15 17:03:51 -0600

    Add files via upload

---
+++ AdditionSubtraction.py
@@ -0,0 +1,14 @@
+print("Hello. Let's add and subtract.")
+num1 = int(input('Input your first number: '))
+num2 = int(input('Input your second number: '))
+
+print(num1, '+', num2, '=', num1 + num2)
+print(num1, '-', num2, '=', num1 - num2)
+
+print("Now let's multiply and divide.")
+
+num3 = int(input('Input your first number: '))
+num4 = int(input('Input your second number: '))
+print(num3, '*', num4, '=', num3 * num4)
+print(num3, '/', num4, '=', num3 / num4)
+print(input('Thanks!'))
